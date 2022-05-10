d37adf4a585e24da9f266405f27b7ff2214502fb
sindhu1490 <103158592+sindhu1490@users.noreply.github.com>
2022-05-10 14:02:02 +0530

    Update deliver.sh

--- scripts/deliver.sh
+++ scripts/deliver.sh
@@ -4,4 +4,4 @@ NAME=`mvn help:evaluate -Dexpression=project.name | grep "^[^\[]"`
 
  VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
 
- java -jar target/${NAME}-${VERSION}.jar
+ java -jar target/${NAME}-${VERSION}.war
